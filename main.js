ad61894e33a12f01510760bb3934803600ac2870
Your Name <joyywamaitha@gmail.com>
2022-10-04 08:37:57 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,54 @@
+let form=document.getElementById("form")
+let input=document.getElementById("input")
+let message=document.getElementById("message")
+let posts = document.getElementById("posts");
+
+form.addEventListener("submit",(e)=>{
+    e.preventDefault();
+    console.log("Button Clicked");
+    formValidation()
+})
+
+let formValidation=()=>{
+    if(input.value=== ""){
+        message.innerHTML="Task can't be blank!"
+    console.log("Failure");
+    } else{
+        console.log("Success");
+        message.innerHTML=""
+        acceptData()
+
+    }
+    
+}
+
+let data={}
+
+let acceptData=()=>{
+    data["text"]=input.value;
+    console.log(data)
+    createPost()
+}
+
+let createPost = () => {
+    posts.innerHTML += `
+    <div>
+      <p>${data.text}</p>
+      <span class="options">
+        <i onClick="editPost(this)" class="fas fa-edit"></i>
+        <i onClick="deletePost(this)" class="fas fa-trash-alt"></i>
+      </span>
+    </div>
+    `;
+    input.value = "";
+  };
+  
+  let deletePost = (e) => {
+    e.parentElement.parentElement.remove();
+  };
+  
+  let editPost = (e) => {
+    input.value = e.parentElement.previousElementSibling.innerHTML;
+    e.parentElement.parentElement.remove();
+  };
+  
